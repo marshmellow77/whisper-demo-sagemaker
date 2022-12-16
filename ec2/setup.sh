2c23821dee0ab923b34d7f5e99fa6bfa934b2502
EC2 Default User <ec2-user@ip-172-16-15-145.ec2.internal>
2022-12-16 05:39:47 +0000

    initial commit

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,10 +1,10 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ~/miniconda
 echo "PATH=$PATH:$HOME/miniconda/bin" >> ~/.bashrc
-sudo apt-get update
-sudo apt install python3-pip
+sudo apt-get update -y
+sudo apt install python3-pip -y
 pip3 install gradio
 pip3 install boto3
 pip3 install iso-639
-sudo apt install ffmpeg
-python3 whisper-demo-sagemaker/ec2/gradio-app.py
+sudo apt install ffmpeg -y
+screen -d -m python3 whisper-demo-sagemaker/ec2/gradio-app.py
